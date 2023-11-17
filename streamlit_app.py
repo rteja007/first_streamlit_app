13ec3267be91a8b9c97babdcd9d3f30ac0d68117
rteja007 <151167755+rteja007@users.noreply.github.com>
2023-11-17 08:02:39 -0500

    Update streamlit_app.py
    added the missing bracket in header line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu'
+
+streamlit.header('Breakfast Menu')
                  streamlit.text('Minapa gaarelu')
                  streamlit.text('Ulli kaaaram')
                  streamlit.text('Coffee its optional')
