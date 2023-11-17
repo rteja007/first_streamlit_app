00a9ee75f8c1938a3148f18a5cf7e5437dd09783
rteja007 <151167755+rteja007@users.noreply.github.com>
2023-11-17 08:04:36 -0500

    Update streamlit_app.py
    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-                 streamlit.text('Minapa gaarelu')
-                 streamlit.text('Ulli kaaaram')
-                 streamlit.text('Coffee its optional')
+streamlit.text('Minapa gaarelu')
+streamlit.text('Ulli kaaaram')
+streamlit.text('Coffee its optional')
