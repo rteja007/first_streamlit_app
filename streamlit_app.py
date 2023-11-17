3662b433cfe03b44a900b614006001f352bbdded
rteja007 <151167755+rteja007@users.noreply.github.com>
2023-11-17 08:01:35 -0500

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu'
+                 streamlit.text('Minapa gaarelu')
+                 streamlit.text('Ulli kaaaram')
+                 streamlit.text('Coffee its optional')
