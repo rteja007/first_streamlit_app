a62a37d0f19de0939fb5c15f5a5725ae9a3e409a
rteja007 <151167755+rteja007@users.noreply.github.com>
2023-11-18 08:30:01 -0500

    Update streamlit_app.py
    Updated menu chages

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Minapa gaarelu')
-streamlit.text('Ulli kaaaram')
-streamlit.text('Coffee its optional')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 🍞 Avacado Toast')
